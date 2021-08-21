91e93edf99230c2324ca4b6a36221676cc49b72e
Nilabh Nikunj <nnikunj@vmware.com>
2021-08-21 17:09:41 +0530

    Jai Hind

--- tasks/scripts/parseBom.py
+++ tasks/scripts/parseBom.py
@@ -7,13 +7,13 @@ import json
 
 rel_bom = open("bom/rel.json")
 parsed_bom_file = json.load(rel_bom)
-f = open("/var/bom-version/version", "w")
+f = open("bom-version/version", "w")
 f.write(parsed_bom_file.get('release-bom').get('current-rel'))
 f.close()
 
 print (parsed_bom_file.get('release-bom').get('current-rel'))
 
 print ("content of file written:\n")
-f = open("/var/bom-version/version", "r")
+f = open("bom-version/version", "r")
 print(f.read())
 f.close()
