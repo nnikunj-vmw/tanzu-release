7fa382f9d4c485565d5ad800ee86d3be3582ee0f
Nilabh Nikunj <nnikunj@vmware.com>
2021-08-21 19:39:22 +0530

    X

--- tasks/scripts/console-print.sh
+++ tasks/scripts/console-print.sh
@@ -8,4 +8,8 @@ ls -lrt $workdir
 echo "---"
 ls -lrt $workdir/..
 echo "ls -lrt bom-version"
+
+ls -lrt $workdir/../bom-version
+echo "workdir/../bom-version/version"
+cat  $workdir/../bom-version/version
  
