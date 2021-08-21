d9c4dcbe98eb4fa6bfae3871cc4f0e3f4ac475f4
Nilabh Nikunj <nnikunj@vmware.com>
2021-08-21 17:28:35 +0530

    file

--- tasks/scripts/parseBom.py
+++ tasks/scripts/parseBom.py
@@ -7,11 +7,16 @@ import json
 
 rel_bom = open("bom/rel.json")
 parsed_bom_file = json.load(rel_bom)
-f = open("bom-version/version", "w+")
+print (parsed_bom_file.get('release-bom').get('current-rel'))
+isExist = os.path.exists("./bom-version")
+if not isExist:
+    os.makedirs("./bom-version")
+    print("O/p dir created")
+f = open("./bom-version/version", "w+")
 f.write(parsed_bom_file.get('release-bom').get('current-rel'))
 f.close()
 
-print (parsed_bom_file.get('release-bom').get('current-rel'))
+
 
 print ("content of file written:\n")
 f = open("bom-version/version", "r")
