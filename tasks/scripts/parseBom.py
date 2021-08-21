a2a8539a20902103b61377c32d7695941573faa3
Nilabh Nikunj <nnikunj@vmware.com>
2021-08-21 19:54:42 +0530

    Change path

--- tasks/scripts/parseBom.py
+++ tasks/scripts/parseBom.py
@@ -16,15 +16,15 @@ print("ZZZZZZZZZZ")
 print (subprocess.Popen("ls -lrt $(pwd)/..", shell=True, stdout=subprocess.PIPE).stdout.read())
 
 print("ZZZZZZZZZZ")
-isExist = os.path.exists("./bom-version")
+isExist = os.path.exists("../bom-version")
 if not isExist:
     os.makedirs("./bom-version")
     print("O/p dir created")
-f = open("./bom-version/version", "w+")
+f = open("../bom-version/version", "w+")
 f.write(parsed_bom_file.get('release-bom').get('current-rel'))
 f.close()
  
 print ("content of file written:\n")
-f = open("bom-version/version", "r")
+f = open("../bom-version/version", "r")
 print(f.read())
 f.close()
