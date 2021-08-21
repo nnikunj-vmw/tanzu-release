f06afc7a948abc3ee1c025832a996aed7c05921c
Nilabh Nikunj <nnikunj@vmware.com>
2021-08-21 19:47:09 +0530

    Parent dir

--- tasks/scripts/parseBom.py
+++ tasks/scripts/parseBom.py
@@ -9,6 +9,12 @@ import shutil
 rel_bom = open("bom/rel.json")
 parsed_bom_file = json.load(rel_bom)
 print (parsed_bom_file.get('release-bom').get('current-rel'))
+cwd = os.getcwd()
+print("Current dir listing")
+os.listdir(cwd)
+print("parent dir listing")
+os.listdir(os.pardir)
+
 isExist = os.path.exists("./bom-version")
 if not isExist:
     os.makedirs("./bom-version")
