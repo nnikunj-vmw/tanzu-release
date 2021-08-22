cf49ec1737bf8fa245fb6b0d49c28b7b825b9939
Nilabh Nikunj <nnikunj@vmware.com>
2021-08-22 13:30:17 +0530

    Pos change

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -5,9 +5,9 @@ echo y | fly -t tanzu set-pipeline \
                 --var "git-private-key=$(cat ~/.ssh/id_rsa)" \
                 --var "docker_user_name=$(cat ~/.ssh/docker_user_name)" \
                 --var "docker_password=$(cat ~/.ssh/docker_password)" \
-                --var "concourse_coordinates=http://192.168.1.6:8080" \
                 --var "concourse_user=admin" \
                 --var "concourse_password=admin" \
+                --var "concourse_coordinates=http://192.168.1.6:8080" \
                 -p lookout -c ./pipeline-tanzu.yaml 
 
 fly -t tanzu up -p lookout
